d8106da41f26d3f20f12c136de432d16e5f1331f
IsraelGosu <gosuisreal2014@gmail.com>
2023-09-08 14:52:10 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints all single numbers of base 10 from 0 going.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+}

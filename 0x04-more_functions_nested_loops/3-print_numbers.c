d6b8d111be50b8ec5a1a1dd09aad13396abf16dd
IsraelGosu <gosuisreal2014@gmail.com>
2023-09-14 14:29:26 -0700

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - this function or code will print all numbers between 0 to 9.
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+	int cc;
+
+	for (cc = 48; cc < 58; cc++)
+	{
+		_putchar(cc);
+	}
+	_putchar('\n');
+}

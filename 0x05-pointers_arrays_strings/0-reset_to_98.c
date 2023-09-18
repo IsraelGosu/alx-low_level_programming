dad6c4a50067fcd37cc5dd2c3a1ee0b32ea3990f
IsraelGosu <gosuisreal2014@gmail.com>
2023-09-18 14:08:09 -0700

    0-reset_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - This function takes a pointer to a parameter and updates its value.
+ * reset_to_98 - This function takes a pointer to a parameter and
+ * then updates its value.
  * @n: input integer.
  * Return: no return.
  */

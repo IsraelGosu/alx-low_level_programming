907a7ba4d22b6505f44295012f679c7845b3f78a
IsraelGosu <gosuisreal2014@gmail.com>
2023-09-27 11:01:05 -0700

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _puts_recursion - A function that prints a string and followed by a new line.
+ * _puts_recursion - A function that prints 
+ * a string and followed by a new line.
  * @s: string
  * Return: no return.
  */

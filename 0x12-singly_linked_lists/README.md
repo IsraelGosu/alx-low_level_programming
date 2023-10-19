d8b5b3fa40b1694937dfb0048a2b821fbc37cb15
IsraelGosu <gosuisreal2014@gmail.com>
2023-10-19 13:24:19 -0700

    Print list

---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+A README file for the project 0x12-singly_linked_lists and this is where i do hard things
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,30 @@
+#ifndef lists_h
+#define lists_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - the structure for a singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: this is the length of the string
+ * @next: this points to the next node
+ *
+ * Description: this is singly linked list node structure
+ * for this project. we do hard things 
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
